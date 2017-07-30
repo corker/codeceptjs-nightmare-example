7699ce6a8454d343f3c0fa991270233dd12c0460
Michael Borisov <mborisov@webbeat.nl>
2017-07-30 13:52:40 +0200

    initial commit

---
+++ tests/example_test.js
@@ -0,0 +1,11 @@
+
+Feature('Example');
+
+Scenario('test something', (I) => {
+  I.amOnPage('/');
+  I.click('.btn-link.float-right');
+  I.click('input.header-search-input');
+  I.fillField('input.header-search-input', 'codecept-nightmare-example');
+  I.pressKey('Enter');
+  I.wait(5);
+});
